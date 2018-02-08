85cc9b4738daaeb11f99a8df814cbf33047b9da3
Verseillie <yverseillie@btslivh.eu>
2018-02-08 14:45:09 +0100

    modif lancement simulateur OBD

--- Ressources/OBDsim/Lancement bluetooth.sh
+++ Ressources/OBDsim/Lancement bluetooth.sh
@@ -1,4 +1,3 @@
-echo "il faut etre root-----------------------------------------"
 hcitool dev
 obdsim --b -g gui_fltk
 rfcomm bind 0 00:18:E7:1E:C6:29 1 # Change this MAC address, putting the MAC of your device

ac7e6c13995bc2bbdc4e218ec3872d0216c5ea9e
YVerseillie <yverseillie@btslivh.eu>
2018-03-22 17:34:06 +0100

    DonneesTR ajout vitesse et regime moteur

--- Codage/Code_de_test/Alfox2/Test_DonneesTR/test/test.ino
+++ Codage/Code_de_test/Alfox2/Test_DonneesTR/test/test.ino
@@ -1,17 +1,45 @@
 #include "DonneesTR.h"
+#include <stdlib.h>
 
 DonneesTR* donneesTR;
 void majDataTR();
 
-int testRegime = 0x410C2B34;       //Seulement pour les tests sans bluetooth
-//int testVitesse = ;
+String testRegime = "010C0FA0";       //Seulement pour les tests sans bluetooth (1000rpm)         /*   /!\   */   à supprimer pour l'intégration
+String testVitesse = "410D64FF";      //vitesse = 100                                             /*   /!\   */   à supprimer pour l'intégration
 
 void setup() {
   Serial.begin(115200);
   donneesTR = new DonneesTR();
-  delay(50);
   majDataTR();
-
+  //-----------------------------------------------------------------------------------------------------------//--------------------------//
+  Serial.print("Vitesse moy : ");                                                                              //                          //
+  Serial.println(donneesTR->getVitesseMoyenne());                                                              //                          //
+  Serial.print("Vitesse max : ");                                                                              //                          //
+  Serial.println(donneesTR->getVitesseMax());                                                                  //                          //
+  Serial.println("==================================");                                                        //  A supprimer :           //
+  Serial.print("Regime moyen :");                                                                              //  utile pour le debug     //
+  Serial.println(donneesTR->getRegimeMoyen());                                                                 //                          //
+  Serial.print("Regime max : ");                                                                               //                          //
+  Serial.println(donneesTR->getRegimeMax());                                                                   //                          //
+  Serial.println("==================================nouvelle valeur d'entrée : vitesse 200 regime 2000");      //                          //
+  //-----------------------------------------------------------------------------------------------------------//--------------------------//
+  testVitesse = "410DC8FF";
+  testRegime = "010C1F40";
+  majDataTR();
+  //-----------------------------------------------------------------------------------------------------------//--------------------------//
+  Serial.print("Vitesse moy : ");                                                                              //                          //
+  Serial.println(donneesTR->getVitesseMoyenne());                                                              //                          //
+  Serial.print("Vitesse max : ");                                                                              //                          //
+  Serial.println(donneesTR->getVitesseMax());                                                                  //                          //
+                                                                                                               //                          //
+  Serial.println("==================================");                                                        //  A supprimer :           //
+  Serial.print("Regime moyen :");                                                                              //  utile pour le debug     //
+  Serial.println(donneesTR->getRegimeMoyen());                                                                 //                          //
+  Serial.print("Regime max : ");                                                                               //                          //
+  Serial.println(donneesTR->getRegimeMax());                                                                   //                          //
+  Serial.println("==================================");                                                        //                          //
+  //-----------------------------------------------------------------------------------------------------------//--------------------------//
+  
   }
 
 void loop() {
@@ -20,19 +48,28 @@ void loop() {
 }
 
 void majDataTR(){
-  
-  //test += 0xffff;             //remplacer test par un obd2->demande(C_REGIME);
-  long a = testRegime>>8;
-  //a >>=8;
-  long b = testRegime<<8;
-  b /= 0x100;
-  //Serial.println(test, HEX);        //DEBUG : affiche les valeurs sans la console
-  //Serial.println(a, HEX);           //DEBUG : affiche les valeurs sans la console
-  //Serial.println(b, HEX);           //DEBUG : affiche les valeurs sans la console
-  long testest = ((a*256)+b)/4;
-  //Serial.print("Régime moteur : ");       //DEBUG : affiche les valeurs sans la console
-  //Serial.print(testest);                  //DEBUG : affiche les valeurs sans la console
-  donneesTR->setRegime(((a*256)+b)/4);
-  
+  donneesTR->setVitesse(lireVitesse());
+  donneesTR->setRegime(lireRegimeMoteur());
+
 }
+
+int lireVitesse(){
+  char trame[testVitesse.length()+1];       //remplacer testVitesse par: obd2->demande(C_VITESSE).length()+1;     /*      /!\      */
+  testVitesse.substring(4, 6).toCharArray(trame,testVitesse.substring(4, 6).length()+1); //remplacer testVitesse par: obd2->demande(C_VITESSE);     /*      /!\      */
+  int a = strtoul(trame,NULL,16);
+  return a;
+}
+
+int lireRegimeMoteur(){
+  char trameA[testRegime.length()+1];        //remplacer testRegime par: obd2->demande(C_REGIME);     /*      /!\      */
+  char trameB[testRegime.length()+1];        //remplacer testRegime par: obd2->demande(C_REGIME);     /*      /!\      */
+  testRegime.substring(4,6).toCharArray(trameA,testRegime.substring(4,6).length()+1);  //remplacer testRegime par: obd2->demande(C_REGIME);     /*      /!\      */
+  testRegime.substring(6,8).toCharArray(trameB,testRegime.substring(6,8).length()+1);  //remplacer testRegime par: obd2->demande(C_REGIME);     /*      /!\      */
+  int a = strtoul(trameA,NULL,16);
+  int b = strtoul(trameB,NULL,16);
+  return ((a*256)+b)/4;
+
+}
+
+
 

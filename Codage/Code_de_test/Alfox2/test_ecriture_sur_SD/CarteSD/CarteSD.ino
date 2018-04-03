d2ccd05196fc7809a0e0389e722fb2da939cf31f
gdesaintdenis <gdesaintdenis@btslivh.eu>
2018-04-03 16:33:08 +0200

    ajout schema

--- Codage/Code_de_test/Alfox2/test_ecriture_sur_SD/CarteSD/CarteSD.ino
+++ Codage/Code_de_test/Alfox2/test_ecriture_sur_SD/CarteSD/CarteSD.ino
@@ -22,10 +22,11 @@ void setup() {
 }
 
 void loop() {
-    carteSD->nouveauFichier("290322.txt");
+    carteSD->nouveauFichier("030418.txt");
     donneesTR->setConsommation(conso);
     conso = conso + 1;
     carteSD->ecrire(donneesTR);
+    //Serial.println(carteSD->supprimerFichier("030418.txt"));
     Serial.println();
     Serial.println();
     delay(1000);

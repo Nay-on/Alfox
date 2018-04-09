97166847ac069638814886fe93837ddcfae1f508
MaxNeves <mneves@btslivh.eu>
2018-04-09 11:53:25 +0200

    réorganisation de mon répertoire perso

---
+++ Gestion/Neves/ORDONNANCEUR/Ordonnanceur_Reel/Main.cpp.ino
@@ -0,0 +1,14 @@
+#include "Bluetooth.h"
+
+Bluetooth* bluetooth = new Bluetooth(2,3);
+
+void setup() {
+  Serial.begin(9600);
+  bluetooth->connexion("18,e7,1ec629");
+  bluetooth->isActif();
+}
+
+void loop() {
+  
+
+}

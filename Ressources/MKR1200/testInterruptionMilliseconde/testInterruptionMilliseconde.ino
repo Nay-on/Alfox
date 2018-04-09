c7ac6c3f03b972dc968cdd9b79abc608ccdc66e7
MaxNeves <mneves@btslivh.eu>
2018-04-09 08:05:34 +0200

    ajout du fichier de configuration de la carte MKR à mon dossier perso

---
+++ Ressources/MKR1200/testInterruptionMilliseconde/testInterruptionMilliseconde.ino
@@ -0,0 +1,66 @@
+/*
+ * Interruption toutes les millisecondes afin de faire un gps->read() toutes les millisecondes
+ * Ouvrir le moniteur série pour voir s'incrémenter le compteur
+ */
+
+volatile int compteur = 0;
+
+
+// Set timer TC4 to call the TC4_Handler every millisecond
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Début du test");
+
+  
+  // Set up the generic clock (GCLK4) used to clock timers
+  REG_GCLK_GENDIV = GCLK_GENDIV_DIV(1) |          // On divise les 48MHz  par 1: 48MHz/1=48MHz
+                    GCLK_GENDIV_ID(4);            // Select Generic Clock (GCLK) 4
+  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+  REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |           // Set the duty cycle to 50/50 HIGH/LOW
+                     GCLK_GENCTRL_GENEN |         // Enable GCLK4
+                     GCLK_GENCTRL_SRC_DFLL48M |   // Set the 48MHz clock source
+                     GCLK_GENCTRL_ID(4);          // Select GCLK4
+  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+  // Feed GCLK4 to TC4 and TC5
+  REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK4 to TC4 and TC5
+                     GCLK_CLKCTRL_GEN_GCLK4 |     // Select GCLK4
+                     GCLK_CLKCTRL_ID_TC4_TC5;     // Feed the GCLK4 to TC4 and TC5
+  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+ 
+  REG_TC4_COUNT16_CC0 = 47999;                    // Le timer compte de 0 à 47999 soit 48000/48000000 = 1 ms, puis déclenche une interruption
+  while (TC4->COUNT16.STATUS.bit.SYNCBUSY);       // Wait for synchronization
+
+  //NVIC_DisableIRQ(TC4_IRQn);
+  //NVIC_ClearPendingIRQ(TC4_IRQn);
+  NVIC_SetPriority(TC4_IRQn, 0);    // Set the Nested Vector Interrupt Controller (NVIC) priority for TC4 to 0 (highest)
+  NVIC_EnableIRQ(TC4_IRQn);         // Connect TC4 to Nested Vector Interrupt Controller (NVIC)
+
+  REG_TC4_INTFLAG |= TC_INTFLAG_OVF;              // Clear the interrupt flags
+  REG_TC4_INTENSET = TC_INTENSET_OVF;             // Enable TC4 interrupts
+  // REG_TC4_INTENCLR = TC_INTENCLR_OVF;          // Disable TC4 interrupts
+ 
+  REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV1 |      // On met le présidviseur à 1. Pourrait valoir DIV2, DIC4, DIV8, DIV16, DIV64, DIV256, DIV1024
+                   TC_CTRLA_WAVEGEN_MFRQ |        // Timer 4 en mode "match frequency" (MFRQ) 
+                   TC_CTRLA_ENABLE;               // Enable TC4
+  while (TC4->COUNT16.STATUS.bit.SYNCBUSY);       // Wait for synchronization
+}
+
+void loop() 
+{
+   //Rien à faire
+}
+
+void TC4_Handler()                              // Interrupt Service Routine (ISR) for timer TC4
+{     
+  
+  // Check for overflow (OVF) interrupt
+  if (TC4->COUNT16.INTFLAG.bit.OVF && TC4->COUNT16.INTENSET.bit.OVF)             
+  {
+    Serial.println(compteur);
+    compteur++;
+    REG_TC4_INTFLAG = TC_INTFLAG_OVF;         // Clear the OVF interrupt flag
+  }
+}

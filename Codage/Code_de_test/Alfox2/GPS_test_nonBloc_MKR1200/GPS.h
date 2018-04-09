3e4cae599d863a2b2429b7a6c227773802aaa6b2
ocommeng <olivier-alain.commenge@ac-toulouse.fr>
2018-04-09 15:12:01 +0200

    Ajout d'un prog de test GPS sur Serial1 de MKR1200

---
+++ Codage/Code_de_test/Alfox2/GPS_test_nonBloc_MKR1200/GPS.h
@@ -0,0 +1,39 @@
+#ifndef __GPS__
+#define __GPS__
+
+#include <Arduino.h>
+#include <Adafruit_GPS.h>
+#include <time.h>
+
+
+class GPS
+{
+
+	private:
+
+		float latitude;
+		float longitude; 
+		struct tm datation;
+    bool available = false;
+    
+    boolean usingInterrupt = false;
+    Adafruit_GPS* gps;
+    HardwareSerial* serialGPS;
+
+
+	public:
+	
+		/** Constructeur **/
+		GPS();
+
+		int maj();
+		float getLatitude() {return latitude;};
+   	float getLongitude() {return longitude;};
+		struct tm getDatation() {return datation;};
+   	bool isDispo();
+    char readDATA();
+    struct tm getDatationUltimate();
+		
+};
+
+#endif

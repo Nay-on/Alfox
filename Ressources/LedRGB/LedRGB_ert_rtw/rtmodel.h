12b5ba35360b7a6418e0941257ca7ae4d7eb5444
ocommeng <olivier-alain.commenge@ac-toulouse.fr>
2018-04-11 16:01:27 +0200

    Ajout d'un modèle Matlab pour gérer les couleur de la led RGB

---
+++ Ressources/LedRGB/LedRGB_ert_rtw/rtmodel.h
@@ -0,0 +1,29 @@
+/*
+ * Primary and Secondary School License - for use in teaching and meeting
+ * course requirements at primary and secondary schools only.  Not for
+ * government, commercial, university, or other organizational use.
+ *
+ * File: rtmodel.h
+ *
+ * Code generated for Simulink model 'LedRGB'.
+ *
+ * Model version                  : 1.9
+ * Simulink Coder version         : 8.12 (R2017a) 16-Feb-2017
+ * C/C++ source code generated on : Sat Apr  7 17:41:21 2018
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_rtmodel_h_
+#define RTW_HEADER_rtmodel_h_
+#include "LedRGB.h"
+#endif                                 /* RTW_HEADER_rtmodel_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */

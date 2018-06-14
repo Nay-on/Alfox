55894cefe0e9c7fdf2b2fb96104a28fe8535c77d
atorreil <atorreilles@btslivh.eu>
2018-06-14 09:51:05 +0200

    Ajout des commentaire et de méthodes pour le bluetooth

--- Codage/Classes/Bluetooth.cpp
+++ Codage/Classes/Bluetooth.cpp
@@ -1,6 +1,7 @@
 
 #include "Bluetooth.h"
 
+//Création des port séries
 Bluetooth::Bluetooth(int pinAlim, int pinEn) 
 {
   this->pinAlim=pinAlim;
@@ -10,16 +11,47 @@ Bluetooth::Bluetooth(int pinAlim, int pinEn)
   pinPeripheral(7, PIO_SERCOM_ALT); //Rx
   pinMode(pinAlim, OUTPUT); //Base Transistor
   pinMode(pinEn, OUTPUT); //EN
-  delay(100);
+  /*delay(100);
   digitalWrite(pinEn, HIGH);
   delay(100);
   digitalWrite(pinAlim, HIGH);
-  delay(1000);
+  delay(1000);*/
   serialBT->begin(38400);
 }
 
+//Allumer ou eteindre le module bluetooth
+void Bluetooth::desactiverModule()
+{
+  digitalWrite(this->pinAlim, LOW);
+}
+
+//Activer le mode configuration ou pas
+void Bluetooth::activerModule(Mode mode)
+{
+  if (mode == commandeAT_ON)
+  {
+      digitalWrite(this->pinEn, HIGH);
+  }  
+  else if (mode == commandeAT_OFF)
+  {
+      digitalWrite(this->pinEn, LOW);  
+  }
+  delay(100);
+  digitalWrite(this->pinAlim, HIGH);
+  delay(3000);
+}
+
+//Configuration du module Bluetooth
 int Bluetooth::connexion(String adresse) 
 {
+
+  //Test commande CMODE=0
+  /*Serial.println("On entre dans la méthode de connexion");
+  this->adresseOBD2=adresse;
+  activerModule(commandeAT_OFF);
+  delay(10000);
+  Serial.println("Le délai est passé");*/
+  
   this->adresseOBD2=adresse;
   if(this->isActif() == true)
   {
@@ -27,8 +59,17 @@ int Bluetooth::connexion(String adresse)
   }
   else
   {
+    
+    //Activation du mode configuration
+    /*Serial.println("On entre dans le else pour relancer la procédure de connexion");
+    desactiverModule();
+    delay(2000);*/
+    activerModule(commandeAT_ON);  
+    
     int sommeErreurs = 0;
 
+    serialBT->flush();
+    delay(1000);
     //reinitialiser();
     sommeErreurs += modeMaster();
     sommeErreurs += modeConnexion();
@@ -57,22 +98,20 @@ int Bluetooth::connexion(String adresse)
   }
 }
 
+//Vérification de la connexion
 bool Bluetooth::isActif() 
 {
   String contenu = "";
   serialBT->println("ATI");
-  /*while (serialBT->available() <= 0){Serial.print(serialBT->read());};*/
+  delay(100);
+
   //contenu = serialBT->readStringUntil('\n');
   delay(5000);//5000 par défaut
-  while (serialBT->available() <= 0);
-  while (serialBT->available() > 0) 
-  {
-    contenu += serialBT->read();
-  }
+  contenu = serialBT->readStringUntil('>');
   
   //Serial.println(contenu);
   
-  if (contenu.substring(0, 3) == "697")
+  if (contenu.substring(0, 3) == "ELM")
   {
     return true;
   }
@@ -84,21 +123,23 @@ bool Bluetooth::isActif()
 
 }
 
-
 Uart* Bluetooth::getLiaisonBT() 
 {
   return serialBT;
 }
 
-int Bluetooth::reinitialiser() 
+//Réinitialisation du module Bluetooth
+void Bluetooth::reinitialiser() 
 {
   serialBT->println("AT+ORGL");
 }
 
+//Changement du mode de fonctionnement
 int Bluetooth::modeMaster() 
 {
   String contenu = "";
   serialBT->println("AT+ROLE=1");
+  delay(100);
   while (serialBT->available() <= 0);
   contenu = serialBT->readStringUntil('\n');
   if (contenu == "OK")
@@ -111,10 +152,12 @@ int Bluetooth::modeMaster()
   }
 }
 
+//Changement du principe de connexion
 int Bluetooth::modeConnexion() 
 {
   String contenu = "";
   serialBT->println("AT+CMODE=1");
+  delay(100);
   while (serialBT->available() <= 0);
   contenu = serialBT->readStringUntil('\n');
   if (contenu == "OK")
@@ -127,7 +170,7 @@ int Bluetooth::modeConnexion()
   }
 }
 
-
+//Changement du mot de passe, par défaut 1234
 int Bluetooth::motDePasse() 
 {
   String contenu = "";
@@ -144,11 +187,12 @@ int Bluetooth::motDePasse()
   }
 }
 
-
+//Initialisation du module bluetooth afin de pouvoir configurer la connexion
 int Bluetooth::initialisation() 
 {
   String contenu = "";
   serialBT->println("AT+INIT");
+  delay(200);
   while (serialBT->available() <= 0);
   contenu = serialBT->readStringUntil('\n');
   if (contenu == "OK" )
@@ -161,11 +205,12 @@ int Bluetooth::initialisation()
   }
 }
 
-
+//Appairage à l'OBD2
 int Bluetooth::appairage(String adresse) 
 {
   String contenu = "";
   Serial.println("Appairage");
+  //serialBT->flush();
   serialBT->println("AT+PAIR=" + adresse + ",10");//10 par défaut
   //delay(5000);
   delay(100);
@@ -175,14 +220,6 @@ int Bluetooth::appairage(String adresse)
     }*/
   contenu = serialBT->readStringUntil('\n');
   Serial.println(contenu);
-  /*delay(5000);//5000 par défaut
-  while (serialBT->available() <= 0);
-  while (serialBT->available() > 0) 
-  {
-    contenu += serialBT->read();
-  }
-  Serial.println(contenu);
-  */
   if (contenu == "OK" )
   {
     return 16;
@@ -193,12 +230,13 @@ int Bluetooth::appairage(String adresse)
   }
 }
 
-
+//Ajout de l'adresse de l'OBD2 dans une liste d'appareil pour le CMODE=0
 int Bluetooth::bind(String adresse) 
 {
   String contenu = "";
   Serial.println("Bind");
   serialBT->println("AT+BIND=" + adresse);
+  delay(5000);
   while (serialBT->available() <= 0);
   contenu = serialBT->readStringUntil('\n');
   Serial.println(contenu);
@@ -212,10 +250,12 @@ int Bluetooth::bind(String adresse)
   }
 }
 
+//Changement du principe de connexion en CMODE=0
 int Bluetooth::modeDeconnecte() 
 {
   String contenu = "";
   serialBT->println("AT+CMODE=0");
+  delay(100);
   while (serialBT->available() <= 0);
   contenu = serialBT->readStringUntil('\n');
   if (contenu == "OK")
@@ -228,11 +268,13 @@ int Bluetooth::modeDeconnecte()
   }
 }
 
+//Connexion manuelle au module OBD2
 int Bluetooth::lien(String adresse) 
 {
   String contenu = "";
   Serial.println("Link");
   serialBT->println("AT+LINK=" + adresse);
+  delay(10000);
   while (serialBT->available() <= 0);
   contenu = serialBT->readStringUntil('\n');
   Serial.println(contenu);

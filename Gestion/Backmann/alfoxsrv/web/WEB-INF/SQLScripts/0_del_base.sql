db3eb9ff0e846974cf462c5f3e108633d01d030f
acros1 <acros@btslivh.eu>
2018-06-15 17:18:06 +0200

    Ajout d'un script qui supprime la BD

---
+++ Gestion/Backmann/alfoxsrv/web/WEB-INF/SQLScripts/0_del_base.sql
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  snir2g1
+ * Created: 15 juin 2018
+ */
+
+drop schema if exists alfox;
